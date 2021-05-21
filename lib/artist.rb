8cabecddf4e2bddbaad4c46131c46f953bac2519
davidhenshaw <davidhenshaw2014@gmail.com>
2021-05-21 14:18:43 -0500

    Completed song, artist, genre portion

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,39 @@
+require 'pry'
 
+class Artist
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def genres
+        ret = []
+        songs.each do |song|
+            if !ret.include?(song.genre)
+                ret << song.genre
+            end
+        end
+        ret
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,28 @@
+class Genre
+    attr_accessor :name, :songs
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @songs = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artists
+        ret = []
+        @songs.each do |song|
+            if !ret.include?(song.artist)
+                ret << song.artist
+            end
+        end
+        ret
+    end
+
+    def add_song(song)
+        @songs << song
+    end
+end
