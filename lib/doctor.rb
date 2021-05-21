2f4f3df7d8f85b82649b048ea75c556064104d4b
davidhenshaw <davidhenshaw2014@gmail.com>
2021-05-21 15:11:55 -0500

    commit tree b478ae76c835cc5487e24936bd4946a9247031dc

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,33 @@
+class Doctor
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |appointment|
+            appointment.doctor == self
+        end
+    end
+
+    def patients
+        ret = []
+        appointments.each do |appointment|
+            if !ret.include?(appointment.patient)
+                ret << appointment.patient
+            end
+        end
+        ret
+    end
+
+    def new_appointment(date, patient)
+        appt = Appointment.new(date, patient, self)
+    end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,27 @@
+require 'pry'
 
+class Patient
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        appt = Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select do |appointment|
+            binding.pry
+            appointment.patient = self
+        end
+    end
+end
